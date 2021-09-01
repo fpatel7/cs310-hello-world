4efc75f9b67612356363f2cccda2445053d282a7
fpatel7 <79761974+fpatel7@users.noreply.github.com>
2021-09-01 08:42:19 -0500

    Commit Message

--- Class1.java
+++ Class1.java
@@ -1,6 +1,6 @@
 /**
  *
- * @author Your Name
+ * @author Fenny Patel
  */
 
 public class Class1 {
@@ -8,11 +8,7 @@ public class Class1 {
     private void f1() {
         // v1.0
     }
-    private void f2() {
-        // v2.0
-    }
-    private void f3() {
-        // v3.0
-    }
+    
+   
 
 }
